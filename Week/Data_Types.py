32d1a6c01d11e36b4f0692c774f695ff0eac0c7a
Vikasvyat <Vikasvyat@gmail.com>
2025-12-09 23:44:26 +0200

    commit python data types

---
+++ Week/Data_Types.py
@@ -0,0 +1,43 @@
+
+age = input ("Enter your age: ")
+print ("Youe age is ",age)
+
+#1. Write a Python expression to calculate the sum of 10 and 3.
+print ( 10+3)
+#2. Subtract 7 from 15 using Python.
+print (15-7)
+#3. Multiply 4 by 8 using Python.
+print (4*8)
+#4. Divide 18 by 3.
+print (18/3)
+#5. Use floor division to find how many whole times 11 can be divided by 4.
+print(11//4)
+#6. Find the remainder when 9 is divided by 4 using the modulus operator.
+print(9%4)
+#7. Raise 6 to the power of 3.
+print(6**3)
+#8. Use the negation operator to make the number 12 negative.
+print (-12)
+
+user_name = 'John Doe'
+user_name_list = list(user_name)
+
+#check what happens to the string:
+print(user_name_list)
+
+#1. Create a list of fruits and print the second fruit of your list.
+fruits = ['pomelo','citron','avokado','apple']
+print (fruits[1])
+#2. Create a set of numbers and try to print the last index of it.
+numbers = {1,2,3,4,5,6}
+print ("set is unordered")
+#3. Create a tuple of 5 Star Wars characters names and print the third name.
+star_war_characters =('yoda','darth','leia','luke')
+print (star_war_characters[2])
+#4. Create a dictionary with the information about a woman called Alice, 
+#23 years old that lives in New York.
+user_info = {
+    "name": "Alice",
+    "age": 23,
+    "city": 'NY'
+}
