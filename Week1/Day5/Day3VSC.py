60a60cce49400645c28f33617c9cfe78af303d10
Vikasvyat <Vikasvyat@gmail.com>
2025-12-21 16:21:33 +0200

    commit exercises+Gold+Ninja for day2

---
+++ Week1/Day5/Day3VSC.py
@@ -0,0 +1,16 @@
+#Access the value of key history
+
+
+sample_dict = { 
+   "class":{ 
+      "student":{ 
+         "name":"Mike",
+         "marks":{ 
+            "physics":70,
+            "history":80
+         }
+      }
+   }
+}
+
+print (sample_dict["class"]["student"]["marks"]["history"]) 
